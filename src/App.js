936b1232de267bda6deedf8bbdee7638ba930c3d
plva <paul.vasiu@gmail.com>
2025-06-24 16:07:01 -0400

    Migrate to React: Hello World setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Algorithm Visualizations</h1>
+        <p>Hello World from React! 🚀</p>
+        <p>This is a test of our React migration.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
